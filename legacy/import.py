01be768b48fc407bba94b3339b91f0363103c676
Nauder <prestes.nauder@gmail.com>
2024-10-19 17:14:24 -0300

    feat: add legacy icon ETL

--- legacy/import.py
+++ legacy/import.py
@@ -2,6 +2,37 @@ import json
 
 from pandas import DataFrame
 
+from legacy_service import fetch_image
+
+
+def sort_sprite_list(sprite_list: list) -> dict:
+    """Sorts a given sprite list by image size"""
+
+    sorted_sprites = {}
+
+    for sprite in sprite_list:
+        print(sprite)
+        sprite_art = fetch_image(sprite, "spt")
+
+        if sprite_art.width == 128:
+            sorted_sprites['small'] = sprite
+        elif sprite_art.width == 256:
+            sorted_sprites['medium'] = sprite
+        elif sprite_art.width == 512:
+            sorted_sprites['large'] = sprite
+
+    return sorted_sprites
+
+
+def sort_icon_sizes(icons: list) -> list:
+    sorted_icons = []
+
+    for icon in icons:
+        print(icon)
+        sorted_icons.append(sort_sprite_list(icon))
+
+    return sorted_icons
+
 
 def import_from_legacy():
 
@@ -39,9 +70,10 @@ def import_from_legacy():
 
         # Icons
         icons = DataFrame()
-        icons.insert(0, 'bundle_1', [data[0] for data in data_legacy['icons'].values()])
-        icons.insert(0, 'bundle_2', [data[1] for data in data_legacy['icons'].values()])
-        icons.insert(0, 'bundle_3', [data[2] for data in data_legacy['icons'].values()])
+        sorted_icons = sort_icon_sizes(data_legacy['icons'].values())
+        icons.insert(0, 'small', [data['small'] for data in sorted_icons])
+        icons.insert(0, 'medium', [data['medium'] for data in sorted_icons])
+        icons.insert(0, 'large', [data['large'] for data in sorted_icons])
         icons.insert(0, 'name', data_legacy['icons'].keys())
         icons.to_parquet('./data/icons.parquet')
 
