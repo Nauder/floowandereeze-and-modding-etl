4d52e0b2eb811ede48c148a7c6c08391353afa35
bea131 <emptyacc0021@gmail.com>
2026-05-08 22:57:28 +1000

    fix: create missing temp folders upon initial run

--- etl/services/data_service.py
+++ etl/services/data_service.py
@@ -7,6 +7,7 @@ from concurrent.futures import ThreadPoolExecutor
 from os.path import isfile
 from typing import Any, Dict, List, Union
 from datetime import datetime
+from pathlib import Path
 
 from pandas import DataFrame, Series
 
@@ -511,3 +512,7 @@ class DataService:
             self.logger.info("Updating Version...")
             with open("./data/version.txt", "w", encoding="utf-8") as file:
                 file.write(datetime.today().strftime("%Y-%m-%d"))
+    
+    def create_temp_folders(self) -> None:
+        path = Path("./etl/services/temp/ids.json")
+        path.parent.mkdir(parents=True, exist_ok=True)
